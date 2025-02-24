ad4e9c506b702f359262d7649391c38d1a06e69e
Renz <140231442+lawrnzzz@users.noreply.github.com.>
2025-02-24 21:55:38 +0800

    add DropdownController to index file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-console.log('Hello, World!');
+export default class DropdownController {
+  constructor({ dropdownBtn, dropdownMenu, arrowIcon }) {
+    this.dropdownBtn = document.querySelector(dropdownBtn);
+    this.dropdownMenu = document.querySelector(dropdownMenu);
+    this.arrowIcon = document.querySelector(arrowIcon);
+
+    if (!this.dropdownBtn || !this.dropdownMenu || !this.arrowIcon) {
+      throw new Error('DropdownController: Invalid selectors provided.');
+    }
+
+    this.#init();
+  }
+
+  #init() {
+    const rootElement = document.documentElement;
+
+    // Toggle when button is clicked
+    this.dropdownBtn.addEventListener('click', (e) => {
+      e.stopPropagation();
+      this.toggle();
+    });
+
+    // Close when clicked outside of dropdown
+    rootElement.addEventListener('click', (e) => {
+      const { target } = e;
+      if (
+        !this.dropdownMenu.contains(target) &&
+        !this.dropdownBtn.contains(target)
+      ) {
+        this.close();
+      }
+    });
+  }
+
+  toggle() {
+    const isOpen = this.dropdownMenu.classList.contains(
+      'dropdown-menu--active'
+    );
+
+    if (isOpen) {
+      this.close();
+    } else {
+      this.open();
+    }
+  }
+
+  open() {
+    this.dropdownMenu.classList.add('dropdown-menu--active');
+    this.arrowIcon.classList.add('arrow--active');
+  }
+
+  close() {
+    this.dropdownMenu.classList.remove('dropdown-menu--active');
+    this.arrowIcon.classList.remove('arrow--active');
+  }
+}
+
+const dropdown = new DropdownController({
+  dropdownBtn: '.dropdown-btn',
+  dropdownMenu: '.dropdown-menu',
+  arrowIcon: '.arrow',
+});
